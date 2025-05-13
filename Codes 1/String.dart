7b7a1111a5ab533d955e67b725ab2dc9be29d055
hayat-se <hayatlucky064@gmail.com>
2025-05-13 23:25:03 +0500

    addinng new file

---
+++ Codes 1/String.dart
@@ -0,0 +1,13 @@
+void main() {
+  String Name = 'Hayat Nabi';
+  print(Name.length); // it prints the length of Name means the number of Letters
+  print(Name[0]); // Index starts from 0 and every letter has an index now on 0 index it will print H.
+  print(Name.toUpperCase());  // built-In function that convert all the alphabets into Upper Case letters
+
+  // _____________+++++++++++++++++++++++++_________________________
+
+  String n1 = '1', n2 = '2';
+
+  String TotalString = n1 + n2; // String Concatination (Combining String/Text)
+  print(TotalString);
+}
---
+++ Codes 1/swap.dart
@@ -0,0 +1,14 @@
+void main() {
+  int x = 10, y = 5, temp;
+  print('Before Swaping :');
+  print('X : $x');
+  print('Y : $y');
+  temp = x;
+  x = y;
+  y = temp;
+
+  print('\nAfter Swaping : ');
+
+  print('X : $x');
+  print('Y : $y');
+}
